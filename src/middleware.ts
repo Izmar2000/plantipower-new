ca60bb3c977278d3fb8be730662687a2b11b5d95
Ramzi <hadad@MBP-van-Ramzi.home>
2026-02-12 13:18:31 +0100

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,14 +7,12 @@ import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
 function getLocale(request: NextRequest): string | undefined {
-    // Negotiator expects plain object so we need to transform headers
     const negotiatorHeaders: Record<string, string> = {}
     request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
     // @ts-ignore locales are readonly
     const locales: string[] = i18n.locales
 
-    // Use negotiator and intl-localematcher to get best locale
     let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
         locales
     )
@@ -36,8 +34,6 @@ export function middleware(request: NextRequest) {
     if (pathnameIsMissingLocale) {
         const locale = getLocale(request)
 
-        // e.g. incoming request is /products
-        // The new URL is now /en/products
         return NextResponse.redirect(
             new URL(
                 `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
@@ -48,6 +44,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    // Matcher ignoring `/_next/` and `/api/`
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|assets).*)'],
+    // Deze matcher negeert alle systeembestanden EN al jouw belangrijke mappen met afbeeldingen/documenten
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|docs|PP Shield info|assets).*)'],
 }
